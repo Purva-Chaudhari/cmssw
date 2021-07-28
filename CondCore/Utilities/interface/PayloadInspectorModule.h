08640f995777876da6b2dfb66159cea11f95f13d
Purva-Chaudhari <48947123+Purva-Chaudhari@users.noreply.github.com>
2021-07-28 12:20:54 +0530

    Update PayloadInspectorModule.h

--- CondCore/Utilities/interface/PayloadInspectorModule.h
+++ CondCore/Utilities/interface/PayloadInspectorModule.h
@@ -10,7 +10,7 @@ namespace py = pybind11;
 #define PAYLOAD_INSPECTOR_MODULE(PAYLOAD_TYPENAME) PYBIND11_MODULE(plugin##PAYLOAD_TYPENAME##_PayloadInspector,m)
 
 #define PAYLOAD_INSPECTOR_CLASS(CLASS_NAME)                                                                    \
-  py::class_<CLASS_NAME,py::base<cond::payloadInspector::PlotBase>>(m, STRINGIZE(PPCAT(plot_, CLASS_NAME)))    \                                                                                                                  \
+  py::class_<CLASS_NAME,cond::payloadInspector::PlotBase>(m, STRINGIZE(PPCAT(plot_, CLASS_NAME)))              \                                                                                                                  \
           .def(py::init<>())                                                                                   \
           .def("process", &CLASS_NAME::process)                                                                \
           .def("payloadType", &CLASS_NAME::payloadType)                                                        \
